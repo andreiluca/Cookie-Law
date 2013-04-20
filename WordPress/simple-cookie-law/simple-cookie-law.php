abf75593f825b181d010ec6e0e9d740c917582f3
Andrei Luca <andrei@andreiluca.com>
2013-04-20 20:36:40 +0300

    Typo fix.

--- WordPress/simple-cookie-law/simple-cookie-law.php
+++ WordPress/simple-cookie-law/simple-cookie-law.php
@@ -8,7 +8,7 @@ Version: 1.0
 
 add_action('wp_print_scripts', 'simple_cookie_law');
 
-if(!function_exists(simple_cookie_law)){
+if(!function_exists('simple_cookie_law')){
 	function simple_cookie_law(){
 		if(is_admin() || is_feed()){ return; }
 		wp_enqueue_script('simple-cookie-law', plugin_dir_url(__FILE__).'js/cookie.law.min.js',  false, false, true);
